09a59dbf856109e444a7959ffecece51325d6bc0
ncampos92 <nlcampos@uc.cl>
2016-05-08 20:32:23 -0300

    estilos y conversiones avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,8 @@ class User < ActiveRecord::Base
 
   #Paperclip
   has_attached_file :avatar,
-  styles: {thumb: ["64x64#", :jpg]}
+  styles: {thumb: ["64x64#", :jpg], original: ['500x500>', :jpg] },
+  convert_options: { thumb: "-quality 75 -strip", original: "-quality 85 -strip" }
 
   validates_attachment :avatar,
   :content_type => { :content_type => ["image/jpg", "image/png"] },
