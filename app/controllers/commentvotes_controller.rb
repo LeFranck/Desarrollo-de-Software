a287fda8dd508ccfad47cc262d55c1d57bd3f524
ncampos92 <nlcampos@uc.cl>
2016-05-08 20:14:27 -0300

    metodo set_commentvote

--- app/controllers/commentvotes_controller.rb
+++ app/controllers/commentvotes_controller.rb
@@ -67,7 +67,7 @@ class CommentvotesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_commentvote
-      @commentvote = Commentvote.find(params[:id])
+      @commentvote = @comment.commentvotes.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
