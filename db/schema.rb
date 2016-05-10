c8a81a8a825f197271d04cee2abb7257bb17e87f
Lucas Pose <lucasp@wificlient-10-200-29-189.uc.cl>
2016-05-10 17:20:52 -0300

    content para project | cambios en seed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160508161824) do
+ActiveRecord::Schema.define(version: 20160510200802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 20160508161824) do
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
     t.string   "title"
+    t.string   "content"
   end
 
   create_table "projectvotes", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Project.create({
-    "title"=>"ej1",
-    "video_link"=>"http://www.youtube.com/",
-    "description"=>"Breve descripcion de un proyecto...",
-    "category_id"=>"1"
-})
-Project.create({
-    "title"=>"ej2",
-    "video_link"=>"http://www.youtube.com/",
-    "description"=>"Breve descripcion de un proyecto...2",
-    "category_id"=>"2"
-})
-Project.create({
-    "title"=>"ej3",
-    "video_link"=>"http://www.youtube.com/",
-    "description"=>"Breve descripcion de un proyecto...3",
-    "category_id"=>"1"
-})
 User.create({
-    "username"=>"E_eq_MC2",
+    "username"=>"EisteinCyborg",
     "firstname"=>"Albert",
     "lastname"=>"Einstein",
     "password"=>"123456",
@@ -33,7 +15,7 @@ User.create({
     "email"=>"aeinstein@example.com",
 })
 User.create({
-    "username"=>"IsaacN",
+    "username"=>"1s44cN3wt0n",
     "firstname"=>"Isaac",
     "lastname"=>"Newton",
     "password"=>"123456",
@@ -71,7 +53,7 @@ Category.create({
     "name"=>"Biologia",
 })
 Comment.create({
-    "content"=>"excelente!",
+    "content"=>"Excelente!",
     "user_id"=>"1",
     "project_id"=>"1",
 })
@@ -79,4 +61,116 @@ Comment.create({
     "content"=>"Muy bueno",
     "user_id"=>"1",
     "project_id"=>"2",
+})
+Project.create({
+    "title"=>"Place holder title",
+    "video_link"=>"http://www.youtube.com/",
+    "description"=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed massa non enim lacinia pulvinar in in nulla. ",
+    "category_id"=>"1",
+    "content"=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+    volutpat odio eget faucibus egestas. Vivamus elementum orci eu dictum
+    tristique. Proin et odio gravida, tincidunt tellus ac, cursus mauris. Fusce
+    dolor odio, pharetra at dui quis, vehicula eleifend augue. Etiam porta
+    condimentum felis id semper. Mauris bibendum justo sed elit scelerisque
+    efficitur. Integer tempus mauris mi, a vehicula dolor maximus a. Aenean quam
+    metus, convallis ut gravida eu, porta rutrum ligula. Duis mattis sed metus
+    non ornare. Pellentesque vestibulum ac nulla in rhoncus. Praesent rutrum
+    quam eu ante hendrerit tempor. Proin faucibus vehicula leo sed ullamcorper.
+        Duis lectus ante, auctor id posuere ac, semper ac nibh. Cras eget purus
+    vitae lorem maximus consequat eu non libero.
+        
+        Nunc placerat nisl a orci ultricies commodo. Phasellus rhoncus vitae
+    tortor eget convallis. Morbi laoreet cursus mi, ut dignissim metus blandit
+    a. Proin a vestibulum risus. Vivamus ornare, leo id viverra congue, risus
+    dolor iaculis orci, in facilisis lectus dolor eget tortor. Lorem ipsum dolor
+    sit amet, consectetur adipiscing elit. Phasellus ultrices cursus mauris ut
+    scelerisque. Aenean fermentum commodo lectus vel interdum. Aenean at
+    tincidunt metus, eget eleifend ligula. Pellentesque porttitor ipsum ut magna
+    aliquet sagittis a et dolor. Quisque tellus lacus, faucibus a maximus ac,
+        maximus eget tortor. Sed consectetur suscipit odio.
+        
+        Morbi sagittis libero sed nunc pharetra, non scelerisque erat volutpat.
+        Aliquam varius dictum tellus a ultricies. Nunc mollis scelerisque massa
+    nec tincidunt. Nullam id risus eu lorem congue ullamcorper sed sed nulla.
+        Morbi lobortis faucibus sem, vitae tincidunt nulla suscipit at. Donec
+    rutrum arcu cursus augue dignissim vestibulum tristique sit amet orci. In
+    hac habitasse platea dictumst. Nulla id pellentesque libero, quis iaculis
+    velit. Cras ut nisi vel sem interdum pretium sit amet sed enim. Donec
+    suscipit fringilla nisi, sed sagittis augue placerat in. Donec tincidunt
+    vehicula elementum."
+
+})
+Project.create({
+    "title"=>"Lorem ipsum dolor sit amet",
+    "video_link"=>"http://www.youtube.com/",
+    "description"=>"Nunc placerat nisl a orci ultricies commodo. Phasellus rhoncus vitae tortor eget convallis. ",
+    "category_id"=>"2",
+    "content"=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+    volutpat odio eget faucibus egestas. Vivamus elementum orci eu dictum
+    tristique. Proin et odio gravida, tincidunt tellus ac, cursus mauris. Fusce
+    dolor odio, pharetra at dui quis, vehicula eleifend augue. Etiam porta
+    condimentum felis id semper. Mauris bibendum justo sed elit scelerisque
+    efficitur. Integer tempus mauris mi, a vehicula dolor maximus a. Aenean quam
+    metus, convallis ut gravida eu, porta rutrum ligula. Duis mattis sed metus
+    non ornare. Pellentesque vestibulum ac nulla in rhoncus. Praesent rutrum
+    quam eu ante hendrerit tempor. Proin faucibus vehicula leo sed ullamcorper.
+        Duis lectus ante, auctor id posuere ac, semper ac nibh. Cras eget purus
+    vitae lorem maximus consequat eu non libero.
+        
+        Nunc placerat nisl a orci ultricies commodo. Phasellus rhoncus vitae
+    tortor eget convallis. Morbi laoreet cursus mi, ut dignissim metus blandit
+    a. Proin a vestibulum risus. Vivamus ornare, leo id viverra congue, risus
+    dolor iaculis orci, in facilisis lectus dolor eget tortor. Lorem ipsum dolor
+    sit amet, consectetur adipiscing elit. Phasellus ultrices cursus mauris ut
+    scelerisque. Aenean fermentum commodo lectus vel interdum. Aenean at
+    tincidunt metus, eget eleifend ligula. Pellentesque porttitor ipsum ut magna
+    aliquet sagittis a et dolor. Quisque tellus lacus, faucibus a maximus ac,
+        maximus eget tortor. Sed consectetur suscipit odio.
+        
+        Morbi sagittis libero sed nunc pharetra, non scelerisque erat volutpat.
+        Aliquam varius dictum tellus a ultricies. Nunc mollis scelerisque massa
+    nec tincidunt. Nullam id risus eu lorem congue ullamcorper sed sed nulla.
+        Morbi lobortis faucibus sem, vitae tincidunt nulla suscipit at. Donec
+    rutrum arcu cursus augue dignissim vestibulum tristique sit amet orci. In
+    hac habitasse platea dictumst. Nulla id pellentesque libero, quis iaculis
+    velit. Cras ut nisi vel sem interdum pretium sit amet sed enim. Donec
+    suscipit fringilla nisi, sed sagittis augue placerat in. Donec tincidunt
+    vehicula elementum."
+})
+Project.create({
+    "title"=>"Tum Torquatus: Prorsus, inquit, assentior",
+    "video_link"=>"http://www.youtube.com/",
+    "description"=>"Morbi sagittis libero sed nunc pharetra, non scelerisque erat volutpat.",
+    "category_id"=>"1",
+    "content"=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+    volutpat odio eget faucibus egestas. Vivamus elementum orci eu dictum
+    tristique. Proin et odio gravida, tincidunt tellus ac, cursus mauris. Fusce
+    dolor odio, pharetra at dui quis, vehicula eleifend augue. Etiam porta
+    condimentum felis id semper. Mauris bibendum justo sed elit scelerisque
+    efficitur. Integer tempus mauris mi, a vehicula dolor maximus a. Aenean quam
+    metus, convallis ut gravida eu, porta rutrum ligula. Duis mattis sed metus
+    non ornare. Pellentesque vestibulum ac nulla in rhoncus. Praesent rutrum
+    quam eu ante hendrerit tempor. Proin faucibus vehicula leo sed ullamcorper.
+        Duis lectus ante, auctor id posuere ac, semper ac nibh. Cras eget purus
+    vitae lorem maximus consequat eu non libero.
+        
+        Nunc placerat nisl a orci ultricies commodo. Phasellus rhoncus vitae
+    tortor eget convallis. Morbi laoreet cursus mi, ut dignissim metus blandit
+    a. Proin a vestibulum risus. Vivamus ornare, leo id viverra congue, risus
+    dolor iaculis orci, in facilisis lectus dolor eget tortor. Lorem ipsum dolor
+    sit amet, consectetur adipiscing elit. Phasellus ultrices cursus mauris ut
+    scelerisque. Aenean fermentum commodo lectus vel interdum. Aenean at
+    tincidunt metus, eget eleifend ligula. Pellentesque porttitor ipsum ut magna
+    aliquet sagittis a et dolor. Quisque tellus lacus, faucibus a maximus ac,
+        maximus eget tortor. Sed consectetur suscipit odio.
+        
+        Morbi sagittis libero sed nunc pharetra, non scelerisque erat volutpat.
+        Aliquam varius dictum tellus a ultricies. Nunc mollis scelerisque massa
+    nec tincidunt. Nullam id risus eu lorem congue ullamcorper sed sed nulla.
+        Morbi lobortis faucibus sem, vitae tincidunt nulla suscipit at. Donec
+    rutrum arcu cursus augue dignissim vestibulum tristique sit amet orci. In
+    hac habitasse platea dictumst. Nulla id pellentesque libero, quis iaculis
+    velit. Cras ut nisi vel sem interdum pretium sit amet sed enim. Donec
+    suscipit fringilla nisi, sed sagittis augue placerat in. Donec tincidunt
+    vehicula elementum."
 })
