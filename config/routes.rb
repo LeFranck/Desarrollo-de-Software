861cb43099a172ddb20cfd1e9b477e0f489ffa6c
ncampos92 <nlcampos@uc.cl>
2016-05-10 12:05:08 -0300

    cambio pagina inicio

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
       resources :commentvotes
     end
   end
-  get 'home/index'
 
   devise_for :users, :skip => [:sessions]
   as :user do
@@ -24,7 +23,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root 'projects#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
