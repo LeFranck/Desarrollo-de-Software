562376d4bba8761b1e3140395edaa59350508b5c
Lucas Pose <posemerani@gmail.com>
2016-06-23 15:53:05 -0400

    fix tabs | funcionan todos menos imagenes

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -2,9 +2,25 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 $(document).on('page:change', function() {
-   $('.nav li a').click(function() {
-      console.log("hola");
-      $('.nav li').removeClass();
-      $(this).closest('li').addClass('active');
-   });
+    $('.nav li a').click(function() {
+        //console.log("hola");
+        $('.nav li').removeClass('active');
+        $(this).closest('li').addClass('active');
+
+
+        var classname = $(this).closest('li').attr('class').split(' ')[0];
+        //var str = '#tabs a[href="#'+classname.toString()+'"]';
+        //$(str).tab('show');
+        $('#tabs a[href="#Imagenes"]').tab('show');
+        //console.log(classname);
+    });
+});
+/* 
+ * Cuando se hace clic en el boton para comentar
+ * Mostrar tab de comentarios
+ * Focus en texto para comentar
+ * */
+$("#ComentarBtn").click(function () {
+    $('#tabs a[href="#Coment"]').tab('show');
+    $('#comment_box').focus();
 });
